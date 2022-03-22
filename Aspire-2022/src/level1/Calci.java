9180ea48616948df74079c506cddc4c94e9fd5a2
shahidnazirmhd <shahidnazirmhd@gmail.com>
2022-03-22 22:43:40 +0530

    22 Mar

--- Aspire-2022/src/level1/Calci.java
+++ Aspire-2022/src/level1/Calci.java
@@ -4,7 +4,7 @@ public class Calci {
 
 	public static void main(String[] args) {
 
-      System.out.println("Calculator Project Loading....");
+      System.out.println("Calculator Project Loading....soon");
 	}
 
 }
