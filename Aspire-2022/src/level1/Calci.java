f2c3d15f1b4123315c4a559a3b9add8e2367cfe7
shahi <shahi@192.168.1.35>
2022-03-22 10:23:08 +0530

    22 Mar

--- Aspire-2022/src/level1/Calci.java
+++ Aspire-2022/src/level1/Calci.java
@@ -3,7 +3,7 @@ package level1;
 public class Calci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
       System.out.println("Calculator Project Loading....");
 	}
 
