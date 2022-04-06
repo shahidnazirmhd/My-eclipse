ad7152db5819f6e35fd1e199c8d25d611d2c9ba0
shahidnazirmhd <shahidnazirmhd@gmail.com>
2022-04-06 16:43:54 +0530

    06 Apr

--- Aspire-2022/src/level1/exceppack/DemoException5.java
+++ Aspire-2022/src/level1/exceppack/DemoException5.java
@@ -2,7 +2,11 @@ package level1.exceppack;
 
 
 public class DemoException5 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		new DemoException5().met();
 		
+	}
+	void met() throws Exception {
+		super.clone();
 	}
 }
