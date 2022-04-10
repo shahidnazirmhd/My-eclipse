02bb99aa96c89bc0d39d2d0fa78484a8cbf575b0
shahidnazirmhd <shahidnazirmhd@gmail.com>
2022-04-10 18:20:20 +0530

    10 Apr

--- Aspire-2022/src/level1/refpack/ReflectionDemo2.java
+++ Aspire-2022/src/level1/refpack/ReflectionDemo2.java
@@ -2,9 +2,14 @@ package level1.refpack;
 public class ReflectionDemo2 {
 	public static void main(String[] args) throws Exception{
 	Hello hello1=new Hello();  // -- static way
-	Hello hello2=(Hello)Class.forName("level1.refpack.Hello").newInstance();   // --Dynamic Way
+	Hello hello2=(Hello)Class.forName("level1.refpack.Hello").newInstance();   // Old--Dynamic Way
+	Hello hello3=(Hello)Class.forName("level1.refpack.Hello").getConstructor().newInstance();  // New--Dynamic Way Constructor is must in class
+	
+	System.out.println(hello3);
           }
 	}	
 class Hello{
-	
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
 }
