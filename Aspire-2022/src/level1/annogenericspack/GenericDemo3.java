516aaedfa4e604c6ffae280a27dd4d50b090cb21
shahidnazirmhd <shahidnazirmhd@gmail.com>
2022-05-11 19:48:27 +0530

    11 May

--- Aspire-2022/src/level1/annogenericspack/GenericDemo3.java
+++ Aspire-2022/src/level1/annogenericspack/GenericDemo3.java
@@ -1,18 +1,26 @@
 package level1.annogenericspack;
 
+import java.lang.reflect.Field;
+
 public class GenericDemo3 {
 
 	public static void main(String[] args) {
 		BrushForPaint<CleanWater> brush=WaterContainer.getBrush();
-		brush.setItem(new CleanWater());
+		System.out.println(brush.getItem());
+		//brush.setItem(new CleanWater());
 		//brush.setItem(new PaintRed());    -- error occured in compile time    **Good programming**
-		CleanWater neer=(CleanWater)brush.getItem();
-		System.out.println(neer);
+		/*
+		 * CleanWater neer=(CleanWater)brush.getItem(); System.out.println(neer);
+		 */
 		BrushForPaint<PaintToPainting> brush2=PaintContainer.getBrush();
-		brush2.setItem(new PaintRed());
+		System.out.println(brush2.getItem());
+		//brush2.setItem(new PaintRed());
+		//brush2.setItem(new PaintYellow());
 		//brush2.setItem(new CleanWater()); -- error occured in compile time    **Good programming**
-		PaintToPainting paint=(PaintToPainting)brush2.getItem();
-		System.out.println(paint);
+		/*
+		 * PaintToPainting paint=(PaintToPainting)brush2.getItem();
+		 * System.out.println(paint);
+		 */
 	}
 
 }
@@ -35,6 +43,7 @@ class DryInAir{}
 	}
 }*/
 class BrushForPaint<T>{
+	@Inject(ClassName ="level1.annogenericspack.PaintRed" )
 	T item;
 
 	public T getItem() {
@@ -47,18 +56,43 @@ class BrushForPaint<T>{
 }
 class PaintContainer{
 	public static BrushForPaint<PaintToPainting> getBrush() {
+		try {
 		BrushForPaint<PaintToPainting> brush1=new BrushForPaint<>();
-		brush1.setItem(new PaintYellow());
+		Class C=brush1.getClass();
+		Field field=C.getDeclaredField("item");
+		field.setAccessible(true);
+		Inject in=(Inject)field.getAnnotation(Inject.class);
+		if(in!=null) {
+			String classname=in.ClassName();
+			PaintToPainting paint=(PaintToPainting)Class.forName(classname).newInstance();
+			brush1.setItem(paint);
+		}
 		return brush1;
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
 class WaterContainer{
 	public static BrushForPaint<CleanWater> getBrush() {
+		try {
 		BrushForPaint<CleanWater> brush2=new BrushForPaint<>();
+		Class C=brush2.getClass();
+		Field field=C.getDeclaredField("item");
+		field.setAccessible(true);
+		Inject in=(Inject)field.getAnnotation(Inject.class);
+		if(in!=null) {
 		brush2.setItem(new CleanWater());
+		}
 		return brush2;
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		}
 	}
-}
+
 
 
 
