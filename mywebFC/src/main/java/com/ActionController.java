856f3fb5334b83dfa77fdae7bde8cc5ac56e2ba1
shahidnazirmhd <shahidnazirmhd@gmail.com>
2022-09-26 12:08:10 +0530

    26 Sep re-commit

--- mywebFC/src/main/java/com/ActionController.java
+++ mywebFC/src/main/java/com/ActionController.java
@@ -14,6 +14,11 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("*.do")
 public class ActionController extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	ReqProcessor rp;
 	@Override
 		public void init(ServletConfig config) throws ServletException {
